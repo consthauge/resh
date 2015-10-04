ca590aed796da70e1a1b3e2759acf12ff334ea60
consthauge <haugeconstanza@gmail.com>
2015-10-04 20:15:06 +0200

    Second commit. Fikset controller

--- WebApi/Controllers/ReshController.cs
+++ WebApi/Controllers/ReshController.cs
@@ -17,27 +17,6 @@ namespace WebApi.Controllers
         {
             var reshUnit = _reshRepository.Get(id);
             return Json(reshUnit, JsonRequestBehavior.AllowGet);
-        }
-
-        #region helpers
-        public string RenderViewToString(string viewName, object model)
-        {
-            ViewData.Model = model;
-            using (var sw = new StringWriter())
-            {
-                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
-                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
-                viewResult.View.Render(viewContext, sw);
-                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
-                return sw.GetStringBuilder().ToString();
-            }
-        }
-        #endregion
-
-        //public ActionResult Get(int id)
-        //{
-        //    var reshUnit = _reshRepository.Get(id);
-        //    return Json(reshUnit, JsonRequestBehavior.AllowGet);
-        //}
+        }        
     }
 }
